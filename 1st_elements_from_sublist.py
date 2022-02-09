26ae34e025832538b66c7a43e01f54e7095e1032
Pro-pravalika <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-09 15:25:46 +0530

    Add files via upload

---
+++ 1st_elements_from_sublist.py
@@ -0,0 +1,4 @@
+def sample(l1):
+    for i in l1:
+        print(i[0])
+sample([[10,25,32,49],[52,58,72,83]])
---
+++ 1st_last_elements_are equal or not.py
@@ -0,0 +1,7 @@
+def sample(l):
+	#for i in l:
+		if l[0]==l[-1]:
+			print(l)
+		else:
+			print('first and last elements are not equal')
+sample([12,3,4,5,6,7,12])
