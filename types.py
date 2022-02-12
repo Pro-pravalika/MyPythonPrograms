6dfef2ee7e95fc67eb119801b343b93fdd653f51
Pravalika Chunarkar <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-13 02:17:15 +0530

    Add files via upload

---
+++ types.py
@@ -0,0 +1,15 @@
+def sample(value):
+	print(type(value))
+sample('pandu')
+
+def sample(num):
+	print(type(num))
+sample(10)
+
+def sample(value):
+	print(type(value))
+sample(10.25)
+
+def sample(str):
+	print(type(str))
+sample(True)
